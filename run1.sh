f65080d0be1d74f835cde3b2b410aa41abc5d6ef
Pankai <st171112@stud.uni-stuttgart.de>
2022-02-16 17:33:49 +0100

    delete run1

--- run1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -l
-
-
-python3 main.py --model_name gru --model_id fc_3 --lowpass_filter 3
-python3 main.py --model_name gru --model_id fc_3 --lowpass_filter 3 
-python3 main.py --model_name gru --model_id fc_3 --lowpass_filter 3 
-python3 main.py --model_name gru --model_id fc_3 --lowpass_filter 3 
-python3 main.py --model_name gru --model_id fc_3 --lowpass_filter 3 
-
--- run2.sh
+++
@@ -1,6 +0,0 @@
-python3 main.py --model_name lstm --model_id lstm
-python3 main.py --model_name lstm --model_id lstm
-python3 main.py --model_name lstm --model_id lstm
-python3 main.py --model_name lstm --model_id lstm
-python3 main.py --model_name lstm --model_id lstm
-
